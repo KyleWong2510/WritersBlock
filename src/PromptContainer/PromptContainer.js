7dd15b5259bedbc176090efb7cb25729b2d98913
Kyle Wong <kylewong2510@gmail.com>
2020-07-29 15:10:52 -0600

    Renders PromptCard

--- src/PromptContainer/PromptContainer.js
+++ src/PromptContainer/PromptContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './PromptContainer.css'
 
+import PromptCard from '../PromptCard/PromptCard'
+
 const PromptContainer = () => {
   return (
     <main className='prompt-container'>
-
+      <PromptCard />
     </main>
   )
 }
