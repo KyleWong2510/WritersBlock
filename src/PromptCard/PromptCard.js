4e31d82041e7281c5bd18c173cfddcab55f0eeee
Kyle Wong <kylewong2510@gmail.com>
2020-07-29 15:57:58 -0600

    Add stories and likes placeholders

--- src/PromptCard/PromptCard.js
+++ src/PromptCard/PromptCard.js
@@ -4,8 +4,11 @@ import './PromptCard.css'
 const PromptCard = () => {
   return (
     <section className='prompt-card'>
-      <h3 className='prompt-desc'>PromptHere</h3>
-      <section className='prompt-stats'>card stats</section>
+      <h3 className='prompt-desc'>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid</h3>
+      <section className='prompt-stats'>
+        <p>X Stories</p>
+        <p>X likes</p>
+      </section>
     </section>
   )
 }
