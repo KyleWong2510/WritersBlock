be692b39e0c7d33cb9cd9b86d834b49182bdbf80
Kyle Wong <kylewong2510@gmail.com>
2020-07-29 17:34:36 -0600

    Create Prompt component

--- src/Prompt/Prompt.js
+++ src/Prompt/Prompt.js
@@ -1,3 +1,16 @@
 import React from 'react'
 import './Prompt.css'
 
+const Prompt = () => {
+  return (
+    <main>
+      <h2>PROMPT</h2>
+      <button>Write this prompt</button>
+      <section>
+        STORIESSTORIESSTORIES
+      </section>
+    </main>
+  )
+}
+
+export default Prompt
