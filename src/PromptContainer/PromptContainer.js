f14710826057985f39c17fe20a3630c9af00764a
Kyle Wong <kylewong2510@gmail.com>
2020-07-29 15:06:13 -0600

    Render container

--- src/PromptContainer/PromptContainer.js
+++ src/PromptContainer/PromptContainer.js
@@ -1,2 +1,12 @@
 import React from 'react'
 import './PromptContainer.css'
+
+const PromptContainer = () => {
+  return (
+    <main className='prompt-container'>
+
+    </main>
+  )
+}
+
+export default PromptContainer
