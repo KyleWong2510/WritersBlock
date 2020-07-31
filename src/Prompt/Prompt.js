046662e1e6edb3a9cf660b0806c325b11d638f73
Kyle Wong <kylewong2510@gmail.com>
2020-07-31 08:47:15 -0600

    Change function for saving to App state

--- src/Prompt/Prompt.js
+++ src/Prompt/Prompt.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import './Prompt.css'
 
-const Prompt = ({ prompt }) => {
+const Prompt = () => {
   return (
     <main className='prompt'>
-      {/* <h2>{prompt.prompt}</h2>
-      <section className='character-details'>
-        <h3>Character</h3>
-        <p className='character-name'>Name: {character.title} {character.first} {character.last}</p>
-        <p>DOB: {date}</p>
-        <p className='character-nationality'>Nationality: {this.state.nationality}</p>
-      </section>
-      <section>
-          <h3>Location</h3>
-          <p>City: {prompt.city}</p>
-          <p>Country: {prompt.country}</p>
-        </section>
+      <h2>PROMPT</h2>
       <button className='write-prompt-btn'>Write this prompt</button>
       <section className='story-container'>
         STORIESSTORIESSTORIES
-      </section> */}
+      </section>
     </main>
   )
 }
--- src/PromptCard/PromptCard.js
+++ src/PromptCard/PromptCard.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './PromptCard.css'
 
-const PromptCard = () => {
+const PromptCard = ({ prompt }) => {
   return (
     <section className='prompt-card'>
-      <h3 className='prompt-desc'>Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid</h3>
+      <h3 className='prompt-desc'>{prompt.prompt}</h3>
       <section className='prompt-stats'>
         <p>X Stories</p>
         <p>X likes</p>
