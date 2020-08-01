b1a161d083892356dc0f328ddc77103489f492ad
Kyle Wong <kylewong2510@gmail.com>
2020-08-01 07:30:22 -0600

    Render prompt data on the Prompt route

--- src/Prompt/Prompt.js
+++ src/Prompt/Prompt.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Prompt.css'
 
-const Prompt = () => {
+const Prompt = ({ prompt }) => {
   return (
     <main className='prompt'>
-      <h2 data-testid='prompt'>PROMPT</h2>
+      {prompt.prompt && 
+        <section>
+          <h2 data-testid='prompt'>Prompt</h2>
+          <p>{prompt.prompt}</p>
+        </section>
+      }
+      {prompt.characterName && 
+        <section>
+          <h3>Character</h3>
+          <p>{prompt.characterName}</p>
+          <p>{prompt.characterAge}</p>
+          <p>{prompt.nationality}</p>
+        </section>
+      }
+      {prompt.location && 
+        <section>
+          <h3>Location</h3>
+          <p>City: {prompt.location.city}</p>
+          <p>Country: {prompt.location.country}</p>
+        </section>
+      }
       <button className='write-prompt-btn'>Write this prompt</button>
       <section data-testid='related-stories' className='story-container'>
         STORIESSTORIESSTORIES
