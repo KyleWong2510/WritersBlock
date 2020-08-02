3b4792652815433082fd07e84857d219bfca2621
Kyle Wong <kylewong2510@gmail.com>
2020-08-02 10:08:06 -0600

    Write prompts and stories POST/GET methods

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -13,3 +13,31 @@ export const getRandomPrompt = () => {
   return fetch('https://ineedaprompt.com/dictionary/default/prompt')
     .then(res => res.json())
 }
+
+export const getPrompts = () => {
+  return fetch('http://localhost:3001/api/v1/prompts')
+    .then(res => res.json())
+}
+
+export const getStories = () => {
+  return fetcch('http://localhost:3001/api/v1/stories')
+    .then(res => res.json())
+}
+
+export const postPrompt = (prompt) => {
+  return fetch('http://localhost:3001/api/v1/prompts', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(prompt)
+  })
+    .then(res => console.log(res))
+}
+
+export const postStory = (story) => {
+  return fetch('http://localhost:3001/api/v1/stories', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(story)
+  })
+    .then(res => console.log(res))
+}
