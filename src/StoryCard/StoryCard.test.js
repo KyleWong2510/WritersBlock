4b5f436c53d0d08f72fa78c4834c3718b667762c
Kyle Wong <kylewong2510@gmail.com>
2020-08-03 12:04:15 -0600

    Pass StoryCard render unit test

--- src/StoryCard/StoryCard.test.js
+++ src/StoryCard/StoryCard.test.js
@@ -2,3 +2,29 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import StoryCard from './StoryCard'
+
+describe('StoryCard', () => {
+  it('should render the title, author, and story', () => {
+    const story = {
+      authorName: "Test Author",
+      promptId: 1596389494922,
+      storyId: 1596389498784,
+      storyText: "Test Story",
+      storyTitle: "Test Title"
+    }
+
+    const { getByText } = render(
+      <BrowserRouter>
+        <StoryCard story={story}/>
+      </BrowserRouter>
+    )
+
+    const title = getByText('Test Title')
+    const author = getByText('Test Author', {exact: false})
+    const storyText = getByText('Test Story')
+    
+    expect(title).toBeInTheDocument()
+    expect(author).toBeInTheDocument()
+    expect(storyText).toBeInTheDocument()
+  })
+})
