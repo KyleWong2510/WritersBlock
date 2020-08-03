ff705f05a718e913ca5d5bc0e21a7de1f8c76fd9
Kyle Wong <kylewong2510@gmail.com>
2020-08-03 10:04:55 -0600

    Style PromptCards

--- src/PromptCard/PromptCard.js
+++ src/PromptCard/PromptCard.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom'
 
 const PromptCard = ({ prompt }) => {
   return (
-    <Link to={`/prompt/${prompt.id}`}>
+    <Link to={`/prompt/${prompt.id}`} className='prompt-card-link'>
       <section className='prompt-card' id={prompt.id}>
-        <h3 data-testid='prompt-desc' className='prompt-desc'>{prompt.prompt}</h3>
+        <p data-testid='prompt-desc' className='prompt-desc'>{prompt.prompt}</p>
         <section data-testid='prompt-stats' className='prompt-stats'>
           <p>X Stories</p>
           <p>X likes</p>
